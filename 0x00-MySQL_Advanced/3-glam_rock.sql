7b6d41f951609400f51d11ef349f70008ec568c8
horlami228 <akintolaolamilekan51@gmail.com>
2024-01-23 23:59:05 +0100

    updated the year

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,6 @@
 
 -- show longetivity up until 2022
 
-SELECT band_name AS band_name, COALESCE (split, 2020) - formed AS lifespan
+SELECT band_name AS band_name, COALESCE (split, 2022) - formed AS lifespan
 FROM metal_bands WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
