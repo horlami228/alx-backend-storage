ec35f7637022df809d5218ed9be4e884f18ddf8a
horlami228 <akintolaolamilekan51@gmail.com>
2024-01-23 21:51:13 +0100

    create new table users

---
+++ 0x00-MySQL_Advanced/main.sql
@@ -0,0 +1,7 @@
+-- USE testing;
+
+-- INSERT INTO users(email, name) VALUES
+--     ('akintolaolamielakn@gmail.com', 'Olamilekan'),
+--     ('akintola51@gmail.com', 'Akintola');
+
+SELECT * FROM users\G;
