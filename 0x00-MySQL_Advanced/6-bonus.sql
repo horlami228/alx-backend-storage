9eb6d41b0683821c73d5277f24c9a9dfd7785122
horlami228 <akintolaolamilekan51@gmail.com>
2024-01-24 14:32:54 +0100

    stored procedure to add new corrections

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,32 @@
+-- Create a stored procedure AddBonus that adds a new correction for a student
+
+DELIMITER // ;
+DROP PROCEDURE IF EXISTS AddBonus;
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT, 
+    IN project_name VARCHAR(255), 
+    IN score float)
+    
+-- BODY OF THE PROCEDURE
+BEGIN
+    DECLARE count_row INT;
+    DECLARE project_id INT;
+
+    SELECT COUNT(*) INTO count_row FROM
+    projects WHERE name = project_name;
+
+    -- CHECK IF THE project_name exist in the projects table
+    IF count_row = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+
+    -- Get ID associated with the project name
+    SELECT id INTO project_id FROM projects
+    WHERE name = project_name;
+
+    -- Insert the new correction
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+
+END//
