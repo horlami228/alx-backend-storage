9d38fe3fdec5a234cea06adea982b714bb593f5f
horlami228 <akintolaolamilekan51@gmail.com>
2024-01-30 22:41:15 +0100

    Revert "update document"
    This reverts commit ed4966f6511c04370bce6eea32773c4e33659d5b.

--- 0x01-NoSQL/9-insert_school.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""insert a new document to the collection"""
-
-def insert_school(mongo_collection, **kwargs):
-    """insert into collection"""
-
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
