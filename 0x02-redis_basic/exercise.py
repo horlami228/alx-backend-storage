63ea2436c6ec1fce04d8debd022923ab46eb88bb
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-02 14:44:25 +0100

    fixed annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,13 +6,15 @@ import uuid
 from typing import Union
 class Cache():
     """This reperesents a cache class"""
-    def __init__(self) -> None:
+    def __init__(self):
+        """Initialize a new cache class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, int, bytes, float]) -> str:
+        """Method for storing data in cache"""
         random_key = str(uuid.uuid4())
 
         self._redis.set(random_key, data)
         return random_key
-    
+  
