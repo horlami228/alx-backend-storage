c4cef4b3a3dc8f3a55a99269b713143410c9ab8b
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-02 23:35:46 +0100

    parametize function to return the data types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 """Writing strings to redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache():
@@ -12,7 +12,7 @@ class Cache():
     def __init__(self):
         """Initialize a new cache class"""
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        # self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method for storing data in cache"""
@@ -20,3 +20,27 @@ class Cache():
 
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float]:
+        """converting to original data type"""
+
+        value = self._redis.get(key)
+
+        if value is not None:
+            if fn:
+                return fn(value)
+
+            else:
+                return value
+        return value
+
+    def get_str(self, key: str) -> str:
+        """retrieve a string value from cache"""
+
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """return an int value from cache"""
+
+        return self.get(key, fn=lambda x: int(x.decode("utf-8")))
